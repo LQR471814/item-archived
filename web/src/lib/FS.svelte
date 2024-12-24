318d8f4f4d8213012e41f42658827204aaee0169
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-12-24 09:06:07 -0800

    wip

--- web/src/lib/FS.svelte
+++ web/src/lib/FS.svelte
@@ -1,12 +1,43 @@
 <script lang="ts">
-import type { Archive } from "../archive"
+  import type { Archive } from "../archive";
+  import { notifyError } from "./error";
+  import { onMount } from "svelte";
 
-let { archive }: {
-  archive: Archive
-} = $props()
+  let {
+    archive,
+  }: {
+    archive: Archive;
+  } = $props();
 
-let cwd = $state<string[]>([])
+  let cursor = $state<string[]>([]);
+
+  type EntryList = {
+    isItem: boolean;
+    name: string;
+  }[];
+
+  let fs = $state<EntryList[]>([]);
+
+  function select(path: string[]) {
+    if (path.length > fs.length) {
+    }
+  }
+
+  onMount(() => {
+    archive.read([]).then((res) => {
+      if (!res.children) {
+        notifyError(new Error("Root entrylist contains null children."));
+        return;
+      }
+
+      const entryList: EntryList = [];
+      for (const itemName of res.children.itemNames) {
+        entryList.push({ isItem: true, name: itemName });
+      }
+      for (const containerName of res.children.containerNames) {
+        entryList.push({ isItem: false, name: containerName });
+      }
+      fs[0] = entryList;
+    });
+  });
 </script>
-
-
-
---
+++ web/src/lib/error.ts
@@ -0,0 +1,3 @@
+export function notifyError(err: Error) {
+  alert(err.message)
+}
